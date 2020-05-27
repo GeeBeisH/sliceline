60fa5261ddbcc25e291e9eaa46cf2ec4634beb40
geebeish <geebeish@mweb.co.za>
2020-05-27 19:16:31 +0200

    Add Navbar, Color and Title styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { createGlobalStyle } from 'styled-components';
+import { Navbar } from './Navbar/Navbar';
 
 const GlobalStyle = createGlobalStyle`
   body {
+	  margin: 0;
 	font-family: 'Open Sans', sans-serif;
   }
 
@@ -15,7 +17,7 @@ function App() {
 	return (
 		<>
 		<GlobalStyle  />
-		<h1>Sliceline</h1>
+		<Navbar />
 		<div>Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased to twinkle. By the same illusion which lifts the horizon of the sea to the level of the spectator on a hillside, the sable cloud beneath was dished out, and the car seemed to float in the middle of an immense dark sphere, whose upper half was strewn with silver. Looking down into the dark gulf below, I could see a ruddy light streaming through a rift in the clouds.	</div> 
 		</>
 	);
---
+++ src/Navbar/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled from 'styled-components';
+import { pizzaRed } from '../Styles/Colors';
+import { Title } from '../Styles/Title';
+
+const NavbarStyled = styled.div`
+	background-color: ${pizzaRed};
+	padding: 10px;
+`;
+
+const Logo = styled(Title)`
+    font-size: 20px;
+    color: #fff;
+    text-shadow: 2px 2px 2.5px #380502;
+
+    display: inline;
+    padding-right: 20px;
+`;
+
+export function Navbar() {
+	return (
+		<NavbarStyled>
+			<Logo>Sliceline 🍕</Logo>
+			Hello Navbar
+		</NavbarStyled>
+	);
+}
